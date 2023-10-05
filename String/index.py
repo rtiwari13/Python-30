f0b99fd51983f28ae794a00252f97f3ddbaebe9b
Ritanshu Tiwari <ritanshutiwari593@gmail.com>
2023-10-05 15:09:56 +0530

    Strings in Python

---
+++ String/index.py
@@ -0,0 +1,30 @@
+# 1.
+# String Data Type 
+
+# declaration of string in Python 
+my_name = "Einstein"
+print(my_name)
+
+#index access
+print(my_name[3])
+
+#index acess in a range 
+print(my_name[0:6])
+
+#type of String 
+print(type(my_name)) 
+
+#single quote is valid inside double quote
+fruit_name = "banana ' Apple ' "
+print(fruit_name)
+
+#double quote inside single quote is valid 
+cat_name = 'lily   "pinky" '
+print(cat_name)
+
+#using same nested quote 
+#need to use a backslash 
+nested_quote='I\'m done'
+print(nested_quote)
+
+
